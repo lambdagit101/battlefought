1f55fc41f237e9af77b5d0bc156158b665dab880
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2022-09-28 19:10:29 +0300

    etyhzdgjadjet
    adgjtadgjadgjadgjsdgjadgj

--- gamemodes/battlefought/gamemode/cl_modules/cl_hud.lua
+++ gamemodes/battlefought/gamemode/cl_modules/cl_hud.lua
@@ -1,3 +1,8 @@
+-- Convars that determine how far away from the edges of the screen the HUD is
+-- Also another one that allows you to disable the cool little effects
+CreateConVar("bfthud_xoffset", 0, FCVAR_ARCHIVE, "How far away from the vertical edges of the screen should the HUD be in pixels?", 0)
+CreateConVar("bfthud_yoffset", 0, FCVAR_ARCHIVE, "How far away from the horizontal edges of the screen should the HUD be in pixels?", 0)
+CreateConVar("bfthud_attention", 1, FCVAR_ARCHIVE, "Determines if special HUD effects are turned on.")
 local hide = {
 	["CHudHealth"] = true,
 	["CHudBattery"] = true,
@@ -6,7 +11,16 @@ local hide = {
     ["CHudSecondaryAmmo"] = true,
     ["CHudSuitPower"] = true,
 }
+hook.Add("HUDShouldDraw", "battle_fought_hudshoulddraw", function(name)
+    if hide[name] then
+		return false
+	end
+end)
 
+--[[
+    Fonts used by the HUD
+    Duplicate fonts with blursize = 2 exist for extra font shadows
+]]
 surface.CreateFont("bfthud_general", {
     font = "Roboto",
     size = ScrW() * (16/1920),
@@ -43,16 +57,7 @@ surface.CreateFont("bfthud_ammosmall_blur", {
     blursize = 2,
 })
 
-CreateConVar("bfthud_xoffset", 0, FCVAR_ARCHIVE, "How far away from the vertical edges of the screen should the HUD be in pixels?", 0)
-CreateConVar("bfthud_yoffset", 0, FCVAR_ARCHIVE, "How far away from the horizontal edges of the screen should the HUD be in pixels?", 0)
-CreateConVar("bfthud_attention", 1, FCVAR_ARCHIVE, "Determines if special HUD effects are turned on.")
-
-hook.Add("HUDShouldDraw", "battle_fought_hudshoulddraw", function(name)
-    if hide[name] then
-		return false
-	end
-end)
-
+-- Most colors used by the hud
 local hudbgcolor = Color(0, 0, 0, 200)
 local hudhpcolor = Color(185, 185, 185)
 local hudhpredcolor = Color(255, 50, 50)
@@ -61,11 +66,13 @@ local hudtxcolor = Color(195, 195, 195)
 local hudtxredcolor = Color(255, 50, 50)
 local hudaxcolor = Color(255, 145, 0)
 
+-- Convenience function added to draw the extra shadow for text without having 2 lines
 local function DrawTextWithShadow(text, font, x, y, color, alignone, aligntwo)
     draw.SimpleText(text, font .. "_blur", x + 2, y + 2, hudbgcolor, alignone, aligntwo)
     draw.SimpleText(text, font, x, y, color, alignone, aligntwo)
 end
 
+-- Sine wave used to make the HP bar pulse red when low on health
 local lowhpsine = math.sin(2 * math.pi * 1 * CurTime())
 
 local displaygradient = Material("vgui/gradient-l.vtf", "noclamp smooth")
@@ -100,35 +107,37 @@ local hudamtxcolor = Color(195, 195, 195)
 local hudamrdcolor = Color(255, 50, 50)
 
 function DrawAmmo()
+    -- Draw nothing if the weapon is not valid
     if not IsValid(LocalPlayer():GetActiveWeapon()) then return end
 
     local ammo1 = LocalPlayer():GetAmmoCount(LocalPlayer():GetActiveWeapon():GetPrimaryAmmoType())
     local maxclip1 = LocalPlayer():GetActiveWeapon():GetMaxClip1()
     local clip1 = LocalPlayer():GetActiveWeapon():Clip1()
 
+    -- Show nothing if the weapon is not meant to have ammo at all
     if clip1 == -1 and ammo1 == 0 then return end
 
+    -- The clip of the weapon is the ammo if the weapon does not use clips
     if clip1 == -1 then
         clip1 = ammo1
     end
 
     DrawTextWithShadow(clip1, "bfthud_ammobig", ScrW() - (ScrW() * 65/1920) - GetConVar("bfthud_xoffset"):GetInt(), ScrH() - (ScrH() * 60/1080) - GetConVar("bfthud_yoffset"):GetInt(), (clip1 < maxclip1 / 3 and hudamrdcolor or hudamtxcolor), TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM)
-    if LocalPlayer():GetActiveWeapon():Clip1() ~= -1 then
+    if LocalPlayer():GetActiveWeapon():Clip1() ~= -1 then -- Don't draw the ammo counter if it's already displayed above
         DrawTextWithShadow(ammo1, "bfthud_ammosmall", ScrW() - (ScrW() * 65/1920) - GetConVar("bfthud_xoffset"):GetInt(), ScrH() - (ScrH() * 60/1080) - GetConVar("bfthud_yoffset"):GetInt(), (ammo1 == 0 and hudamrdcolor or hudamtxcolor), TEXT_ALIGN_RIGHT, TEXT_ALIGN_TOP)
     end
 end
 
 local oldar = 0
-local oldhp = 100
 local showarmor = false
 hook.Add("Think", "battle_fought_armorsound", function()
+    -- Armor isn't the same as last frame? Initiate Operation Glass Break
     if LocalPlayer():Armor() ~= oldar and LocalPlayer():Armor() == 0 then
         surface.PlaySound("common/warning.wav")
         surface.PlaySound("physics/glass/glass_sheet_break" .. math.random(1, 3) .. ".wav")
         showarmor = true
     end
     oldar = LocalPlayer():Armor()
-    oldhp = LocalPlayer():Health()
 end)
 
 local drawarmorfor = 0
