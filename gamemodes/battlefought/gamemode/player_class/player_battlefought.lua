63f4bd97458e51bd15584e318f2baf6eca711a67
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2022-09-28 18:58:51 +0300

    sfghfgjfgh

--- gamemodes/battlefought/gamemode/player_class/player_battlefought.lua
+++ gamemodes/battlefought/gamemode/player_class/player_battlefought.lua
@@ -4,7 +4,7 @@ local PLAYER = {}
 
 PLAYER.DisplayName = "battle_fought_player"
 PLAYER.WalkSpeed = 200
-PLAYER.SlowWalkSpeed = 150
+PLAYER.SlowWalkSpeed = 100
 PLAYER.RunSpeed  = 345
 PLAYER.CrouchedWalkSpeed = 0.3
 PLAYER.StartArmor = 33
