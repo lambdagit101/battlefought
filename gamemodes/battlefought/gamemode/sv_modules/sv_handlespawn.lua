4322b7c5e1c56ff2f825bfec73ec2a30398f2a6f
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2022-09-28 18:45:10 +0300

    temp

--- gamemodes/battlefought/gamemode/sv_modules/sv_handlespawn.lua
+++ gamemodes/battlefought/gamemode/sv_modules/sv_handlespawn.lua
@@ -2,6 +2,23 @@ hook.Add("PlayerSpawn", "battle_fought_main_spawn", function(ply)
     player_manager.SetPlayerClass(ply, "player_battlefought")
 end)
 
+function GM:SpawnHeli()
+    GAMEMODE.heli = ents.Create("bf_dropcopter")
+    GAMEMODE.heli:SetPos(Vector(-2034.973755, 5310.209473, 2576.591064))
+    GAMEMODE.heli:SetAngles(Angle(-0.725239, -90.927902, 0.000000))
+    GAMEMODE.heli:Spawn()
+
+    for k, v in ipairs(player.GetAll()) do
+        v:Spectate(OBS_MODE_CHASE)
+        v:SpectateEntity(GAMEMODE.heli)
+        v:SetNWBool("battle_fought_in_heli", true)
+    end
+end
+
+concommand.Add("spawnheli", function(ply, cmd, args)
+    GAMEMODE:SpawnHeli()
+end)
+
 function GM:PlayerSetModel(ply)
     ply:SetModel("models/player/odessa.mdl")
 
